86345983208f30b988ccbf73d49ea95abd1e95e7
Leandro-Pinho <leandroessencia@gmail.com>
2023-07-17 11:21:54 -0300

    criando as rotas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Write from './pages/Write';
+import Home from './pages/Home';
+import Single from './pages/Single';
+
+// routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>this is Home!</div>,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  // {
+  //   path: "/write",
+  //   element: <Write />,
+  // },
+  // {
+  //   path: "/home",
+  //   element: <Home />,
+  // },
+  // {
+  //   path: "/single",
+  //   element: <Single />,
+  // },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
